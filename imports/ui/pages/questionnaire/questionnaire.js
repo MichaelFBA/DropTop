0ee4290389ecc3a6937b9a3e1ce224eb919bb9b1
Michael <michael@falsebehavinganimals.com>
2016-10-11 07:44:13 +0200

    added in animation

--- imports/ui/pages/questionnaire/questionnaire.js
+++ imports/ui/pages/questionnaire/questionnaire.js
@@ -1,5 +1,4 @@
 import { Template } from 'meteor/templating';
-import { $ } from 'meteor/jquery';
 import './questionnaire.html'
 
 Template.questionnaire.helpers({
@@ -11,14 +10,6 @@ Template.questionnaire.helpers({
             { tag: 'vr'},
             { tag: 'streaming'},
         ]
-    },
-    dynamicWidth: function(){
-        var width = 0;
-        $('.overflow-sideways .tag').each(function(){
-            if($(this).width() === 0) return;
-            width += $(this).width();
-        })
-        return width;
     },
     destroyed: function(){
 
