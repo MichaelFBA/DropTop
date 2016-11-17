072e7849dfc01f0335d51286525760ceacf59457
oddie <arcadeoddie@gmail.com>
2016-11-18 05:24:17 +1100

    Removed not needed body.js

--- client/main.js
+++ client/main.js
@@ -4,5 +4,3 @@ import '/imports/startup/client';
 
 import '../imports/startup/accounts-config.js';
 
-//tutorial said to place this in but need to see if it's already around
-import '../imports/ui/body.js'; //is this actually needed?
