84facd57c76152523b48ed7a057214481c1cabb3
Michael <michael@falsebehavinganimals.com>
2016-11-30 21:07:33 +0100

    Fixed up

--- mup.js
+++ mup.js
@@ -1,34 +1,37 @@
 module.exports = {
   servers: {
     one: {
-      host: '130.211.89.59',
-      username: 'michael'
-      // pem:
-      // password:
-      // or leave blank for authenticate from ssh-agent
+      host: '13.54.2.251',
+      username: 'ubuntu',
+      pem: '/Users/michael/.ssh/droptop.pem', // mup doesn't support '~' alias for home directory
+      // password: 'password',
+      // or leave blank to authenticate using ssh-agent
+      opts: {
+          port: 22,
+      },
     }
   },
 
   meteor: {
     name: 'droptop',
-    path: '../droptop',
+    path: '/Users/michael/sites/droptop', // mup doesn't support '~' alias for home directory
+    dockerImage: 'abernix/meteord:base',
     servers: {
-      one: {}
+      one: {} // list of servers to deploy, from the 'servers' list
     },
     buildOptions: {
       serverOnly: true,
+      debug: true,
+      cleanAfterBuild: true, // default
     },
     env: {
-      ROOT_URL: 'http://droptop.odd13.com',
+      ROOT_URL: 'http://ec2-13-54-2-251.ap-southeast-2.compute.amazonaws.com',
       MONGO_URL: 'mongodb://localhost/meteor'
     },
-
-    dockerImage: 'abernix/meteord:base',
-//      dockerImage: 'meteorhacks/meteord:onbuild',
-    deployCheckWaitTime: 60
+    deployCheckWaitTime: 60 // default 10
   },
 
-  mongo: {
+  mongo: { // (optional)
     oplog: true,
     port: 27017,
     servers: {
